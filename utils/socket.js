8e0bc1b53307ffec9402b8c52913451f48605235
Dharani-ctrl <sridhardharanidharan444@gmail.com>
2026-06-02 23:09:20 +0530

    socket file updated

--- utils/socket.js
+++ utils/socket.js
@@ -6,21 +6,23 @@ module.exports = {
   init: (httpServer) => {
     io = new Server(httpServer, {
       cors: {
-        origin: "*", // allow all for dev
-        methods: ["GET", "POST", "PUT", "DELETE"]
+        origin: [
+          "http://localhost:3000",
+          "https://food-waste-reduction-client.vercel.app"
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
       }
     });
 
     io.on('connection', (socket) => {
       console.log('Client connected:', socket.id);
-      
-      // Clients can join a room using their user ID to receive direct notifications
+
       socket.on('join_room', (userId) => {
         socket.join(userId);
         console.log(`Socket ${socket.id} joined room ${userId}`);
       });
-      
-      // NGOs can join a general 'ngo_room' to get all new food alerts
+
       socket.on('join_ngo_room', () => {
         socket.join('ngo_room');
         console.log(`Socket ${socket.id} joined ngo_room`);
@@ -33,6 +35,7 @@ module.exports = {
 
     return io;
   },
+
   getIO: () => {
     if (!io) {
       throw new Error('Socket.io not initialized!');
